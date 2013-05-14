119746095b8aa9f8068bebdce99fe23fa85f1503
Really Simple <andy@reallysimpleworks.com>
2013-05-14 11:57:10 +0100

    Home page machine images fixed.

--- app/webroot/js/bootstrap.js
+++ app/webroot/js/bootstrap.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 	
 	//	Home page machine parts
 	if ($('#machine').length) {
-		var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+		var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 			rand_arr = [],
 			ext = (Modernizr.svg) ? 'svg' : 'png';
 		for (var i in arr) {
