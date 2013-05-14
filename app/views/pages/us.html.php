72d89448022d38299a72bc8543d7ae206e871e91
Really Simple <andy@reallysimpleworks.com>
2013-05-14 13:02:15 +0100

    Updated Us page.

--- app/views/pages/us.html.php
+++ app/views/pages/us.html.php
@@ -19,21 +19,22 @@
 	
 		<section class="col-3-1">
 			<h3>Us</h3>
-			<p>If you were to visit our studio right now, you’d probably find us tinkering away with something. Making stuff with pixels or wires or paper, or just about anything we can get our hands on. This is what we love doing, and we pour that energy and excitement into every project we undertake.</p>
+			<p>If you were to visit our studio right now, you’d probably find us tinkering away with something, making things with pixels, wires or paper. This is what we love doing, and we pour that energy and excitement into every project we undertake.</p>
 			<p>Fieldwork was founded in 2012 by Loz Ives and Andy Gott, a pair of experienced makers, drawn together by a mutual desire to produce things that help organisations further their work.</p>
 		</section>
 		
 		<section class="col-3-1">
 			<h3>Our Approach</h3>
-			<p>We build things with you, collaboratively, to support and enhance the work that you do. Combining your knowledge of your organisation and it’s work with our experience and process, we make things that have a real impact.</p>
-			<p>When it comes to exploring the possibilities and having new ideas, making things is more effective than thinking about things. We start sketching and prototyping at the very beginning.</p>
+			<p>We build things with you, collaboratively, to support and enhance the work that you do. Combining your knowledge of your organisation and its work, with our experience and design process, we produce things that have real impact.</p>
+			<p>We build things with you, collaboratively, to support and enhance the work that you do. Combining your knowledge of your organisation and its work, with our experience and design process, we produce things that have real impact.</p>
 		</section>
 		
 		
 		<section class="col-3-1">
 			<h3>Things we’re good at</h3>
-			<p>We’re a small team with a strong network of talented collaborators. This allows us to adapt according to specific requirements for a wide variety of projects.</p>
-			<p>We specialise in digital and web products, and branding projects.</p>
+			<p>We’re a focused team of makers and do-ers who specialise in crafting engaging experiences across digital, web and brand.</p>
+			<p>With a strong network of talented collaborators from a range of disciplines, we can adapt according to specific requirements for a wide variety of projects.</p>
+			<p>Whether it’s a big, intricate digital platform for a large organisation, or a simple project for a small start-up, we have the experience and network to do it well.</p>
 		</section>
 		
 	</div>
@@ -42,17 +43,26 @@
 		
 	<div class="primary-section">
 		<h3>Process</h3>
-		<p class="centered-intro runner">Through our work on various digital and branding projects, we’ve developed a solid process for collaboratively exploring problems, and delivering effective solutions.</p>
+		<p class="centered-intro runner">Through our work on various digital and branding projects, we’ve developed a process for collaboratively exploring problems, and delivering effective solutions.</p>
 		<ul class="process-overview grid">
 			<li class="learn col-3-1">
+				<figure>
+					<img src="/img/studio/process-learn.svg" alt="" />
+				</figure>
 				<h4>Learn</h4>
 				<p>Together, we learn about your organisation and the project’s context, define goals, devise a high level strategy, and outline a scope of work.</p>
 			</li>
 			<li class="make col-3-1">
+				<figure>
+					<img src="/img/studio/process-make.svg" alt="" />
+				</figure>
 				<h4>Make</h4>
 				<p>We prototype ideas, test elements of the project, figure out how the things we’re making will work as a larger system, and refine the successful parts.</p>
 			</li>
 			<li class="deliver col-3-1">
+				<figure>
+					<img src="/img/studio/process-deliver.svg" alt="" />
+				</figure>
 				<h4>Deliver</h4>
 				<p>Finally, we develop and deploy final products and assets, and work out how best to launch the project for the most positive impact.</p>
 			</li>
