4411d867d353dcea993ed0e353aa7c8bcf373897
Really Simple <andy@reallysimpleworks.com>
2013-05-17 09:07:27 +0100

    Fixed typo.

--- app/views/users/posterThanks.html.php
+++ app/views/users/posterThanks.html.php
@@ -8,7 +8,7 @@
 	<h1>For you, <?= $name; ?>, anything!</h1>
 	
 	
-	<p class="centered-intro runner">Consider it done. Your Fieldwork poster will soon be winging it’s way to you. In case you’d like to follow us on Twitter, you’ll find us at <a href="http://twitter.com/madebyfieldwork">@madebyfieldwork</a></p>
+	<p class="centered-intro runner">Consider it done. Your Fieldwork poster will soon be winging its way to you. In case you’d like to follow us on Twitter, you’ll find us at <a href="http://twitter.com/madebyfieldwork">@madebyfieldwork</a></p>
 	
 	
 	
