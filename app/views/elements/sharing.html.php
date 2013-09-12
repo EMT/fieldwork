665ad949051608ead031c26bd36757c9c1c871b4
Really Simple <andy@reallysimpleworks.com>
2013-09-12 19:00:17 +0100

    Fixed image URLs in sharing links.

--- app/views/elements/sharing.html.php
+++ app/views/elements/sharing.html.php
@@ -5,14 +5,14 @@
 			'text' => $this->title(), 
 			'related' => 'madebyfieldwork')); ?></li>
 		<li><?= $this->share->facebook(array(
-			'picture' => $this->share->baseUrl() . $main_img, 
+			'picture' => $main_img, 
 			'name' => $this->title(),
 			'description' => '')); ?></li>
 		<li><?= $this->share->pinterest(array(
-			'media' => $this->share->baseUrl() . $main_img, 
+			'media' => $main_img, 
 			'description' => '')); ?></li>
 		<li><?= $this->share->tumblr(array(
-			'source' => $this->share->baseUrl() . $main_img, 
+			'source' => $main_img, 
 			'caption' => $this->title() . '. ' . $description)); ?></li>
 	</ul>
 </div>
